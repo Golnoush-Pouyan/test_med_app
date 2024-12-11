e3bda5c1e52d5c7de16af8baacede5e2767dddee
Golnoush-Pouyan <orchideh.pouyan@gmail.om>
2024-12-11 13:15:23 -0500

    Updated db.js with MongoDB connection details

--- src/server/db.js
+++ src/server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:<PEDHRhtldaVb03xn3TnBMAgd>@172.21.162.233:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
